18f7a61ed5cfdddfff33c30cd06d43a7894f888b
kimzuni <me@kimzuni.com>
2026-07-09 23:09:48 +0900

    refactor: link button/badge icon 기능 통일 및 named group 적용
    - label을 선택적으로 변경 및 이에 따른 size 기본값 자동 설정
    - children 허용
    - 스타일 업데이트

--- src/components/link-badge.tsx
+++ src/components/link-badge.tsx
@@ -6,10 +6,15 @@ import { Icon, type IconName } from "@/components/icon";
 
 
 
-export interface LinkBadgeProps extends Omit<React.ComponentProps<typeof Badge>, "render" | "children"> {
-	icon?: IconName;
-	label: string;
+export interface LinkBadgeProps extends Omit<React.ComponentProps<typeof Badge>, "render"> {
+	label?: string;
 	href?: string;
+	icon?: IconName;
+	iconPosition?: "left" | "right";
+	iconScale?: number;
+	iconRotate?: number;
+	iconTranslateX?: number;
+	iconTranslateY?: number;
 	active?: boolean;
 	activeIcon?: IconName;
 }
@@ -19,13 +24,19 @@ export interface LinkBadgeProps extends Omit<React.ComponentProps<typeof Badge>,
 export const EXTERNAL_REGEX = /^https?:\/\//i;
 
 export function LinkBadge({
-	icon,
 	label,
 	href,
+	icon,
+	iconPosition = "left",
+	iconScale = 1,
+	iconRotate = 0,
+	iconTranslateX = 0,
+	iconTranslateY = 0,
 	active,
 	activeIcon,
 	onClick,
 	variant = "secondary",
+	children: _children,
 	...props
 }: LinkBadgeProps) {
 	icon ??= href && EXTERNAL_REGEX.test(href) ? "ExternalLink" : "Link";
@@ -40,15 +51,32 @@ export function LinkBadge({
 			onClick={onClick}
 			data-active={active}
 			className={cn(
+				"group/link-badge [&>svg]:size-[1em]!",
 				isInteractive && "transition-all",
 				isInteractive && variant === "default" && "hover:bg-primary/80! data-[active=true]:bg-primary/80!",
 				isInteractive && variant === "destructive" && "hover:bg-destructive/70! data-[active=true]:bg-destructive/70!",
 				isInteractive && variant === "secondary" && "hover:text-primary data-[active=true]:text-primary",
-				isInteractive && variant === "ghost" && "bg-transparent!",
+				isInteractive && variant === "ghost" && "overflow-visible bg-transparent!",
+				isInteractive && variant === "link" && "overflow-visible",
+				iconPosition === "right" ? "flex-row-reverse" : "flex-row",
 			)}
 		>
-			<Icon icon={isInteractive && active ? activeIcon : icon}/>
-			{label}
+			<Icon
+				icon={isInteractive && active ? activeIcon : icon}
+				className={cn(
+					"group-hover/link-badge:translate-x-(--tx) group-hover/link-badge:translate-y-(--ty) group-hover/link-badge:rotate-(--rotate)",
+					"group-hover/link-badge:scale-(--scale)",
+					"transition-transform",
+				)}
+				style={{
+					"--tx": `${iconTranslateX}px`,
+					"--ty": `${iconTranslateY}px`,
+					"--scale": iconScale,
+					"--rotate": `${iconRotate}deg`,
+				} as React.CSSProperties}
+			/>
+			<span className="empty:hidden">{label}</span>
+			{_children}
 		</Comp>
 	);
 
